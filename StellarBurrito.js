d70cf3a5c741f1a05eb9ca891a255f3136cddeb0
andrea <andrea@MBP-di-andrea.lan>
2019-03-08 19:29:53 +0100

    first commit

--- StellarBurrito.js
+++ StellarBurrito.js
@@ -9,14 +9,8 @@ let offerOperations=require('./offerOperations.js')
 let StellarSdk=require('stellar-sdk')
 let Account =require('./account')
   module.exports={
-    accountOperations,
-    Pay,
-    history,
-    ledger,
-    assetOperations,
-    accountOptions,
-    accountStatus,
-    offerOperations,
     StellarSdk,
-    Account
+    Account,
+    Fee,
+    Asset
   }
