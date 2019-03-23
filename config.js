d10928d3834c1ac5883f085f179a514023c174ef
Andrea <andrea@tests-MacBook-Pro.local>
2019-03-23 20:59:11 +0100

    remove gitignore files

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-'env':'testnet',
-'testnet_horizon':'https://horizon-testnet.stellar.org',
-'pubnet_horizon':'https://horizon.stellar.org',
-'testaccount':'SDFRBHDKSRBVD73GJ6U6FF4RSBUNXCGKB35DZCZAUMQEJNXBGCWCDKAS',
-'base_reserve':'1'
-}
